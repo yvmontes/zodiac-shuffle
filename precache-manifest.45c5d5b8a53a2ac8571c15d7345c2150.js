e4e19cd71200c76dca1c87d6c229849cab92c53e
Yvette <yvmontes@gmail.com>
2019-06-12 11:46:30 -0700

    Updates

---
+++ precache-manifest.45c5d5b8a53a2ac8571c15d7345c2150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fe01396155206f4efb2065d27f6d63",
+    "url": "/zodiac-shuffle/index.html"
+  },
+  {
+    "revision": "a73eabe3666fdd8d5b1d",
+    "url": "/zodiac-shuffle/static/css/main.24f3df58.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/zodiac-shuffle/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a73eabe3666fdd8d5b1d",
+    "url": "/zodiac-shuffle/static/js/main.647375c6.chunk.js"
+  },
+  {
+    "revision": "6b395cf68c011c6ef04f",
+    "url": "/zodiac-shuffle/static/js/runtime~main.02f0ebc5.js"
+  },
+  {
+    "revision": "5275c568d03f29ab577deb55443d6471",
+    "url": "/zodiac-shuffle/static/media/marble.5275c568.png"
+  }
+]);
